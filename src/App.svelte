4a2ac27772eab657a265896ed1b0a0fb0ce01536
Edward Kang <44958279+ekang7@users.noreply.github.com>
2024-02-06 23:02:43 -0500

    added a prelimary selected list component

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   import TimeGrid from '@event-calendar/time-grid';
   import Interaction from '@event-calendar/interaction'; 
   import addEvent from '@event-calendar/core';
+  import SelectedList from './lib/components/SelectedList.svelte';
 
   // Parameters for Calendar Component
   // maps people to their list of events
@@ -851,11 +852,20 @@
       }
     ]
   };
+  let selected_members = ["Olivia", "Alex", "Elena"]; // array of selected members
+  let hover_members = ["Adam"]; // array of members that are hovered over
   let select_bool = false;
   let ec;
   let plugins = [TimeGrid, Interaction];
   // the following line is inspired by ChatGPT
-  let allEvents = Object.values(people_events).reduce((acc, events) => acc.concat(events), []);
+  let allEvents = Object.values(people_events).reduce((acc, events) => {
+                    if (selected_members.includes(events[0].resourceIds[0])) {
+                        return acc.concat(events);
+                    } else {
+                        return acc;
+                    }
+                    }, []);
+
   let options = {
       view: 'timeGridWeek',
       allDaySlot: false,
@@ -896,71 +906,71 @@
     <button type="submit">Submit</button>
   </form>
 {:else}
-<main>
-
-<!-- Toggle Day or Week View -->
+  <SelectedList {selected_members} {hover_members}/>
+  <main>  
+  <!-- Toggle Day or Week View -->
   {#if options.view == 'timeGridWeek'}
     <button on:click= {() => {options.view = 'timeGridDay'}} >Day View</button>
   {:else}
     <button on:click= {() => {options.view = 'timeGridWeek'}} >Week View</button>
-  {/if}
+    {/if}
 
-  <!-- Attempting to add a new event to calendar -->
-  <!-- <button on:click={addEvent(
-    {
-      id: 10, 
-      start: new Date(2024, 2, 4, 11:00),z
-      end: new Date(2024, 02, 04, 11:30)
+    <!-- Attempting to add a new event to calendar -->
+    <!-- <button on:click={addEvent(
+      {
+        id: 10, 
+        start: new Date(2024, 2, 4, 11:00),z
+        end: new Date(2024, 02, 04, 11:30)
+      }
+      )}>Change slot duration</button> -->
+
+    <!-- Imported Calendar Component -->
+    <Calendar bind:this = {ec} {plugins} {options} />
+
+    <!-- Counter Component -->
+    <div class="card">
+      <Counter />
+    </div>
+
+  </main>
+
+  <style>
+    /* .logo {
+      height: 6em;
+      padding: 1.5em;
+      will-change: filter;
+      transition: filter 300ms;
     }
-    )}>Change slot duration</button> -->
-
-  <!-- Imported Calendar Component -->
-  <Calendar bind:this = {ec} {plugins} {options} />
-
-  <!-- Counter Component -->
-  <div class="card">
-    <Counter />
-  </div>
-
-</main>
-
-<style>
-  /* .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  } */
-</style>
+    .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.svelte:hover {
+      filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
+    .read-the-docs {
+      color: #888;
+    } */
+  </style>
 
 
 
-<!-- BOILER PLATE in MAIN
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1> -->
+  <!-- BOILER PLATE in MAIN
+    <div>
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <img src={viteLogo} class="logo" alt="Vite Logo" />
+      </a>
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <h1>Vite + Svelte</h1> -->
 
-  <!-- <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p> -->
+    <!-- <p>
+      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    </p> -->
 
-  <!-- <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p> -->
+    <!-- <p class="read-the-docs">
+      Click on the Vite and Svelte logos to learn more
+    </p> -->
 
   {/if}
---
+++ src/lib/components/SelectedList.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let selected_members; // array of selected members
+  export let hover_members; // array of members being hovered over
+</script>
+
+<style>
+  /* Add your component styles here */
+    .selected {
+        font-weight: bold; 
+        text-decoration: underline;
+    }
+</style>
+
+{#each selected_members as member}
+  <ol class="selected">{member}</ol>
+{/each}
+
+{#each hover_members as member}
+  <ol>{member}</ol>
+{/each}
+
